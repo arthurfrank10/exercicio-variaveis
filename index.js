d612af4a9136fffa38575e555c59453f16c37b91
arthurfrank10 <142546310+arthurfrank10@users.noreply.github.com>
2023-08-22 21:47:15 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+let variavelTexto = "texto";
+let variavelNumero = 6;
+console.log(typeof variavelTexto);
+console.log(typeof variavelNumero);
+
+//variavel vazia
+
+let variavelVazia;
+console.log(typeof variavelVazia);
+let variavelNula = null;
+console.log(variavelNula);
+
+let a = "b";
+console.log(a);
+console.log(typeof a);
+
+a = null
+console.log(a);
+console.log(typeof a);
+
+//exercicio2
+
+let nome;
+let idade;
+let numero = 6;
+
+nome = prompt("qual o seu nome?");
+idade = prompt("qual a sua idade?");
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(numero + 5);
+console.log(numero.toString() + 5);
+
+//conversao numero
+let n = "42";
+console.log(Number (n));
+console.log(typeof n);
+console.log(typeof Number(n));
