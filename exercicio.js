7c6082d1dc03f040e2d307cd664517cb33d018fe
arthurfrank10 <142546310+arthurfrank10@users.noreply.github.com>
2023-08-22 21:46:10 -0300

    Create exercicio.js

---
+++ exercicio.js
@@ -0,0 +1,18 @@
+/*
+* Arthur
+*
+*/
+
+/*interpretaçao:
+*1. resposta:
+*no primeiro "console.log" sera impresso o valor de b, "10", depois o valor foi alterado para 5. logo, no segundo sera impresso o valor de "a,b", 10,5.
+*
+*2. resposta:
+*diz que o valor de c é igual o de a, o valor de b = c, e o valor de a = b, logo voltara "10,10,10"
+*
+*3. resposta:
+*nomes -cargaHoraria, -valorDia.
+*esse comando coloca o valor das variaveis conforme as respostas do usuario, e retorna o calculo de quanta o mesmo ganha por hora.
+*
+*escrita de codigo:
+*
